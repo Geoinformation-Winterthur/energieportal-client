fd41f05511419971708730c38cc98eb07123d411
benjamin-thormann-win <111492868+benjamin-thormann-win@users.noreply.github.com>
2024-01-19 15:37:10 +0100

    WIP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
-import { StartseiteComponent } from './pages/startseite/startseite.component';
 import { MeineLiegenschaftComponent } from './pages/meine-liegenschaft/meine-liegenschaft.component';
 import { UeberUnsComponent } from './pages/ueber-uns/ueber-uns.component';
 import { KontaktComponent } from './pages/kontakt/kontakt.component';
 
 export const routes: Routes = [
-    {path: 'startseite', component: StartseiteComponent},
     {path: 'meine-liegenschaft', component: MeineLiegenschaftComponent},
     {path: 'ueber-uns', component: UeberUnsComponent},
     {path: 'kontakt', component: KontaktComponent}
--- src/app/pages/startseite/startseite.component.ts
+++ src/app/pages/startseite/startseite.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavigationComponent } from '../../components/navigation/navigation.component';
+import { IntroComponent } from '../../components/intro/intro.component';
 
 @Component({
   selector: 'app-startseite',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavigationComponent, IntroComponent],
   templateUrl: './startseite.component.html',
   styleUrl: './startseite.component.scss'
 })
