d4af1d6456a03e5901bd5f4070ae22b695ce3f63
benjamin-thormann-win <111492868+benjamin-thormann-win@users.noreply.github.com>
2024-01-31 11:13:26 +0100

    added tab component (WIP)

---
+++ src/app/components/tab/tab.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tab',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tab.component.html',
+  styleUrl: './tab.component.scss'
+})
+export class TabComponent {
+
+}
--- src/app/pages/meine-liegenschaft/meine-liegenschaft.component.ts
+++ src/app/pages/meine-liegenschaft/meine-liegenschaft.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppComponent } from '../../app.component';
+import { TabComponent } from '../../components/tab/tab.component';
 
 @Component({
   selector: 'app-meine-liegenschaft',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TabComponent],
   templateUrl: './meine-liegenschaft.component.html',
   styleUrl: './meine-liegenschaft.component.scss'
 })
