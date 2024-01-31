4a915a8e91adcb3457e74245ce6cd84777f23968
benjamin-thormann-win <111492868+benjamin-thormann-win@users.noreply.github.com>
2024-01-31 15:29:48 +0100

    cleanup tab

--- src/app/pages/meine-liegenschaft/meine-liegenschaft.component.ts
+++ src/app/pages/meine-liegenschaft/meine-liegenschaft.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppComponent } from '../../app.component';
-import { TabComponent } from '../../components/tab/tab.component';
 import { TabsComponent } from '../../components/tabs/tabs.component';
 
 @Component({
   selector: 'app-meine-liegenschaft',
   standalone: true,
-  imports: [CommonModule, TabComponent, TabsComponent],
+  imports: [CommonModule, TabsComponent],
   templateUrl: './meine-liegenschaft.component.html',
   styleUrl: './meine-liegenschaft.component.scss'
 })
