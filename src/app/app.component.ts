dfb907eab48cb9f048856599b2107f525bd7f1e8
benjamin-thormann-win <111492868+benjamin-thormann-win@users.noreply.github.com>
2024-01-31 13:01:15 +0100

    removed unnecessary startseite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,13 @@ import { InfoboxComponent } from './components/infobox/infobox.component';
 import { InteraktiveGrafikComponent } from './components/interaktive-grafik/interaktive-grafik.component';
 import { IntroComponent } from './components/intro/intro.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
-import { StartseiteComponent } from './pages/startseite/startseite.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, NavigationComponent, IntroComponent, StartseiteComponent]
+    imports: [CommonModule, RouterOutlet, NavigationComponent, IntroComponent]
 })
 export class AppComponent {
   title = 'energieportal-client';
